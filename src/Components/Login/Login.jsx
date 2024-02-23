06b97d77795ce6caeaf2105840ad8e55071c7953
neerajmj <nmj41198@gmail.com>
2024-02-23 19:57:37 +0530

    client-23

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,18 +1,55 @@
 import React, { useState } from "react";
-import './Login.css'
+import './Login.css' 
+import axios from "axios";
+import Content from "../Content/Content";
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <div className="App">
+      {!isLoggedIn ? (
+        <Login handleLoginSuccess={handleLoginSuccess} />
+      ) : (
+        <Content />
+      )}
+    </div>
+  );
+};
 const Login = () => {
-  const [username, setusername] = useState('')
-  const [password, setpassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async(event)=>{
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4102/login',{
+        username:username,
+        password:password
+      })
+      if(response.data.success){
+        alert("login sucessfull")
+      }else{
+        alert("Invalid login")
+      }
+    } catch (error) {
+      alert("an error occured ")
+    }
+  }
   return (
     <>
       <div className="wrapper">
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <h1>Login</h1>
           <div className="input-box">
-            <input type="text" placeholder="Username" required="" />
+            <input type="text1" placeholder="Username" value={username} onChange={(e)=>setUsername(e.target.value)} required="" />
           </div>
           <div className="input-box">
-            <input type="password" placeholder="Password" required="" />
+            <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} required="" />
           </div>
           <button type="submit" className="btn">
             Login
