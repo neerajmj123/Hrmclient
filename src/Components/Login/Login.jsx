13cdbe1d43984ff25a6e44801dfc5e343d642feb
neerajmj <nmj41198@gmail.com>
2024-02-29 16:04:25 +0530

    client-29

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -6,10 +6,25 @@ import axios from 'axios';
 function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [emailerror,setEmailError] = useState('');
+  const [passworderror,setPasswordError] =useState('');
   const navigate = useNavigate();
+
+    const email_val =/^[a-z0-9-_]*@([a-z0-9]+)*(\.[a-z]{})/;
+    const password_val=/^[a-z0-9_@\.]{6,}$/;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setEmailError();
+    setPasswordError();
+
+
+if(!email_val.test(email)){
+  setEmailError("Email error")
+}
+if(!password_val.test(password)){
+  setPasswordError("password must be 6 characters")
+}
     try {
       const response = await axios.post('http://localhost:3000/login', {
         email,
@@ -40,10 +55,12 @@ function Login() {
         <form action="" onSubmit={handleSubmit}>
           <h1>Login</h1>
           <div className="input-box">
-            <input type="email" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} required="" />
+            <input type="email" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} required=""/>
+            {emailerror && <div className="error">{emailerror}</div>}
           </div>
           <div className="input-box">
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required="" />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required=""/>
+            {passworderror && <div className="error">{passworderror}</div>}
           </div>
           <button type="submit" className="btn">Login</button>
         </form>
