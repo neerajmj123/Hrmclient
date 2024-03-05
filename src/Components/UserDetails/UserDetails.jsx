ea11d4b61c933ec2542b3e7fdc43518558679310
neerajmj <nmj41198@gmail.com>
2024-03-05 11:18:10 +0530

    client-5

--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {useParams} from 'react-router-dom'
+import { BrowserRouter as Router, Route,Routes,Link,useParams} from 'react-router-dom';
 import './UserDetails.css'
 import axios from "axios";
 function UserDetails(){
     const {userId} =useParams();
     const [user,setUser]=useState(null);
-    const[editable,setEditable]=useState(false);
-    const [updatedUser,setUpdatedUser]=useState({});
+    const [readOnly,setReadOnly]=useState(true);
 
     useEffect(()=>{
         const fetchData = async()=>{
@@ -19,49 +18,44 @@ function UserDetails(){
         };
         fetchData();
     },[userId]);
-    const handleEdit = () => {
-      setEditable(true);
-    }
-    const handleSubmit = async(e) => {
-      e.preventDefault();
-      try {
-        await axios.put(`http://localhost:3000/updateUser/${userId}`,updatedUser);
-        setEditable(false);
-      } catch (error) {
-        console.error("error updating user details",error);
-      }
-    }
-    const handleChange = (e)=>{
-      const {name,value}=e.target;
-      setUpdatedUser({...updatedUser,[name]:value});
-    };
     if(!user){
         return <h1>Loading......</h1>
     }
+    const ToggleReadonly=()=>{
+      setReadOnly(prevState => !prevState)
+    }
+    const handleSubmit = async(e) => {
+      try {
+        await axios.put(`http://localhost:3000/updateUser/${userId}`,user);
+        console.log("Data updated succesfully")
+      } catch (error) {
+        console.log("error updating user details",error);
+      }
+    }
     return(
         <div className="form-container">
-      <form  className="form" onSubmit={handleSubmit}>
+      <form  className="form">
         <div className="form-group">
           <label htmlFor="Name" className="userlabel">Name</label>
-          <input type="text"id="name"placeholder='Name' value={ editable ? updatedUser.name || user.name : user.name } onChange={handleChange} readOnly={!editable}/>
+          <input type="text"id="name"placeholder='Name' defaultValue={user ? user.name : '' }  readOnly={readOnly} onChange={(e)=>setUser({...user,name:e.target.value})}/>
         </div>
         <div className="form-group">
           <label htmlFor="Email" className="userlabel">Email</label>
-          <input type="email"id="email"placeholder='Email' value={ editable?updatedUser.email ||  user.email : user.email} onChange={handleChange} readOnly={!editable}/>
+          <input type="email"id="email"placeholder='Email' defaultValue={ user ? user.email :'' }  readOnly={readOnly} onChange={(e)=>setUser({...user,email:e.target.value})}/>
         </div>
         <div className="form-group">
           <label htmlFor="Phone_no" className="userlabel">Phone Number</label>
-          <input type="tel"id="phoneNumber"placeholder='Phone Number' value={ editable?updatedUser.phone_no || user.phone_no : user.phone_no}onChange={handleChange} readOnly={!editable}/>
+          <input type="tel"id="phoneNumber"placeholder='Phone Number' defaultValue={ user ? user.phone_no : ''} readOnly={readOnly} onChange={(e)=>setUser({...user,phone_no:e.target.value})}/>
         </div>
         <div className="form-group">
           <label htmlFor="Pincode" className="userlabel">Pincode</label>
-          <input type="text"id="pincode" placeholder='Pincode' value={ editable?updatedUser.pincode || user.pincode:user.pincode} onChange={handleChange} readOnly={!editable}/>
+          <input type="text"id="pincode" placeholder='Pincode' defaultValue={ user ? user.pincode:''}  readOnly={readOnly} onChange={(e)=>setUser({...user,pincode:e.target.value})}/>
         </div>
-        {editable ?(
-        <button type="submit">Submit</button>
-        ):(
-        <button type="button" onClick={handleEdit} className="edit">Edit</button>
-        )}
+        
+       <Link to="/list"><button type="submit" onClick={handleSubmit}>Submit</button></Link>
+      
+        <button type="button" onClick={ToggleReadonly} className="edit">Edit</button>
+    
       </form>
     </div>
     )
